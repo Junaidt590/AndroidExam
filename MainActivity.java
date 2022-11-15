0ccc2f1e4cabde91a7fbc8f7c89f39e1b675f18e
Junaidt590 <94429845+Junaidt590@users.noreply.github.com>
2022-11-15 10:48:16 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.op1:
+                Intent i = new Intent(this, MainActivity2.class);
+                startActivity(i);
+
+
+            case R.id.op2:
+                Intent intent = new Intent(this, MainActivity3.class);
+                startActivity(intent);
+
+
+        }
+        return false;
+    }
+}
